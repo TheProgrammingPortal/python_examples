c608ffa12d45761e97c4e33b3402563d768ef17f
TheProgrammingPortal <theprogrammingportal@gmail.com>
2020-11-21 22:41:13 +0530

    initial commit - screenshot code added

--- Screenshot.py
+++ Screenshot.py
@@ -1,11 +1,15 @@
-# import pyautogui, time
-# time.sleep(5)
-# screeshot = pyautogui.screenshot()
-# screeshot.save('image1.png')
-# print('screenshot taken.')
-#
+# Method - 1 --> using pyautogui module
+
+import pyautogui, time
+time.sleep(5)
+screeshot = pyautogui.screenshot()
+screeshot.save('image1.png')
+print('screenshot taken using pyautogui.')
+
+# Method - 2 --> using Pillow module
 
 from PIL import ImageGrab
 image = ImageGrab.grab(bbox=(100,100,500,800))
 image.save('image2.png')
+print('screenshot taken using Pillow.')
 
