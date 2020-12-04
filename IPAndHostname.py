4abcc3ff6a9ec36f3f946f892b5f464968d448d7
TheProgrammingPortal <theprogrammingportal@gmail.com>
2020-12-04 20:30:19 +0530

    initial commit - IP address and Hostname in Python

--- IPAndHostname.py
+++ IPAndHostname.py
@@ -1,12 +1,12 @@
 import socket
 try:
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
+    hostname = socket.gethostname() # returns hostname
+    ip_address = socket.gethostbyname(hostname)  # returns IPv4 address with respect to hostname
     print('Hostname :', hostname)
     print('IP Address :', ip_address)
-    fqdn = socket.getfqdn('www.google.com')
+    fqdn = socket.getfqdn('www.google.com') # returns fully qualified domain name for name
     print('FQDN', fqdn)
-    print(socket.gethostbyname_ex(hostname))
+    print(socket.gethostbyname_ex(hostname)) # Return a triple (hostname, aliaslist, ipaddrlist)
 
 except:
     print('error while getting hostname or invalid hostname!')
